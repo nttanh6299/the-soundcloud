eb5ae6470228447ef418df3c715118c4f7e2d9e8
Anh Nguyen Tran <anh.nguyentran@exe.com.vn>
2020-02-08 21:01:36 +0700

    configure store react redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={configureStore()}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
