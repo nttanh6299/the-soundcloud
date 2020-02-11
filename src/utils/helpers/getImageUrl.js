1752f9896b0e79649492070aafb224afdb487c62
Anh Nguyen Tran <anh.nguyentran@exe.com.vn>
2020-02-11 12:51:01 +0700

    fixing get image url function

--- src/utils/helpers/getImageUrl.js
+++ src/utils/helpers/getImageUrl.js
@@ -3,6 +3,8 @@ import { IMAGE_SIZE } from '../../constants/GlobalConstants';
 // sample url: https://i1.sndcdn.com/artworks-000064260922-w3cv4b-large.jpg
 
 export function getImageUrl(url, size = '') {
+  if (!url) return null;
+
   switch (size) {
     case IMAGE_SIZE.XLARGE:
       return url.replace('large', IMAGE_SIZE.XLARGE);
